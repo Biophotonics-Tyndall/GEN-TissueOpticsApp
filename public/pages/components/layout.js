108cdb59a2517ba865eece5bf23b13777011cb93
jsmatias <smatias.jean@gmail.com>
2021-06-03 23:05:55 +0100

    layout partially created

---
+++ public/pages/components/layout.js
@@ -0,0 +1,19 @@
+var currentUrlArr = window.location.href.split("/")
+var root = (currentUrlArr[currentUrlArr.length - 2] === 'pages') ? "./" : "./pages/"
+
+document.write(`
+    <div class="navbar">
+        <div class="navbar-title">
+            Tissue Optics App
+        </div class="navbar-menu">
+            <div class="navbar-item"><a href="../index.html">Home</a></div>
+            <div class="navbar-item"><a href="${root}tissue_abs.html">Tissue absorption</a></div>
+            <div class="navbar-item"><a href="${root}tissue_abs_inv.html">Inverse tissue absorption</a></div>
+            <div class="navbar-item"><a href="${root}tissue_mueff.html">Effective attenuation coefficient</a></div>
+            <div class="navbar-item"><a href="${root}tissue_DE.html">Diffusion equation</a></div>
+        </div>
+    <div class="footer">
+        <p>Tissue Optic App</p>
+        <div>Developed by: B. Jayet and J.S. Matias</div>
+    </div>
+`);
